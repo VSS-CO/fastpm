cf7eb42136408d0e178c81f5ec61c39f37246ee5
VSSCO <161714242+kessud2021@users.noreply.github.com>
2025-12-30 16:47:11 +0530

    Create utils.go

---
+++ src/extracter/utils.go
@@ -0,0 +1,10 @@
+package extractor
+
+import "os"
+
+func EnsureDir(path string) error {
+    if _, err := os.Stat(path); os.IsNotExist(err) {
+        return os.MkdirAll(path, 0755)
+    }
+    return nil
+}
